5fcf22022102eb56cfdac295580a933b216f2e30
GoncharenkoK <99185069+GoncharEkaterina@users.noreply.github.com>
2022-05-15 23:09:29 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,21 @@
 
 template<typename T, int size>
 class TStack {
-
+ private:
+   T arr[50];
+   int temp;
+ public:
+   TStack(): temp(-1) {}
+    bool isEmpty() { return temp == -1; }
+    bool isFull() { return temp == size-1; }
+    void pop() {
+        if (!isEmpty()) --temp;
+    }
+    T get() { return arr[temp]; }
+    void push(T data) {
+        if (!isFull())
+           arr[++temp] = data;
+        else throw "Full!";
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
